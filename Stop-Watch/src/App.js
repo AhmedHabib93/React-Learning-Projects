a4b3e2f559ff5dbe4f1b9f40c4af47d6b9253997
AhmedHabib93 <166270473+AhmedHabib93@users.noreply.github.com>
2024-10-08 13:59:40 +0300

    optmize some code

--- Stop-Watch/src/App.js
+++ Stop-Watch/src/App.js
@@ -18,8 +18,6 @@ export default function App() {
       let interval;
       if (isStart) {
         interval = setInterval(() => setTime((prev) => prev + 10), 10);
-      } else {
-        clearInterval(interval);
       }
       return () => clearInterval(interval);
     },
@@ -27,22 +25,32 @@ export default function App() {
   );
 
   return (
-    <div className="py-4 max-w-md flex flex-col justify-center items-center m-auto">
-      <h2 className="font-bold">Stop-Watch</h2>
-      <div className="py-2 px-4 bg-blue-500  text-white m-4 rounded-lg space-x-1">
-        <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:</span>
+    <div className="py-8 max-w-48 flex flex-col justify-center items-center mx-auto">
+      <h2 className="font-bold text-red-500">Stop - Watch</h2>
+      <div className="py-2 px-4 bg-blue-500 w-full text-center  text-white m-4 rounded-lg space-x-1">
+        <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)} :</span>
+        <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)} :</span>
         <span>{("0" + (Math.floor(time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div className="flex gap-8 justify-evenly w-full">
+      <div className="flex gap-8 justify-between w-full">
+        {isStart ? (
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+            onClick={handleClick}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+            onClick={handleClick}
+          >
+            Start
+          </button>
+        )}
+
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleClick}
-        >
-          {isStart ? "Stop" : "Start"}
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
           onClick={handleReset}
         >
           Reset
