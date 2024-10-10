f8ff743301a9880715c6d070b7db28edd100bc1b
AhmedHabib93 <166270473+AhmedHabib93@users.noreply.github.com>
2024-10-10 18:33:17 +0300

    small replacement

--- 11 - worldwise/src/contexts/FakeAuthContext.jsx
+++ 11 - worldwise/src/contexts/FakeAuthContext.jsx
@@ -16,10 +16,10 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Ahmed",
+  email: "Ahmed@example.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "https://i.pravatar.cc/100?u=xx",
 };
 
 function AuthProvider({ children }) {
